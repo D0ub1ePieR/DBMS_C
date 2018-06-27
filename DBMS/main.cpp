92765b45a8de4a45cfc9d082e0c619dac8c5d9d3
D0ub1ePieR <535447508@qq.com>
2018-06-27 21:33:58 +0800

    config : 微调部分模块

--- DBMS/main.cpp
+++ DBMS/main.cpp
@@ -25,10 +25,10 @@ int main(int argc, char *argv[])
 	QObject::connect(&dm, SIGNAL(goto_stuinfo()), &info, SLOT(infoshow()));		//主页面跳转至学生信息查询页面
 	QObject::connect(&dm, SIGNAL(goto_stuclass()), &cl, SLOT(classshow()));		//主页面跳转至学生课程查询页面
 	QObject::connect(&dm, SIGNAL(goto_stuchoose()), &in, SLOT(getmain()));	//主页面跳转至选课页面
-	QObject::connect(&in, SIGNAL(sendnum(string)), &ch, SLOT(chooseshow(string)));
+	QObject::connect(&in, SIGNAL(sendnum(string)), &ch, SLOT(chooseshow(string)));		//选课系统学号输入
 	QObject::connect(&ch, SIGNAL(refresh(string)), &ch, SLOT(chooseshow(string)));		//选课系统自动刷新
-	QObject::connect(&cf, SIGNAL(configrefresh()), &cf, SLOT(configshow()));
-	QObject::connect(&dm, SIGNAL(goto_config()), &log, SLOT(login()));
+	QObject::connect(&cf, SIGNAL(configrefresh()), &cf, SLOT(configshow()));		//系统管理页面自动刷新
+	QObject::connect(&dm, SIGNAL(goto_config()), &log, SLOT(login()));			//系统管理页面跳转登录
 	QObject::connect(&log, SIGNAL(loginsuccess()), &cf, SLOT(configshow()));	//主页面跳转至管理系统
 	QObject::connect(&info, SIGNAL(infoback()), &dm, SLOT(get_info()));		//学生信息返回跳转
 	QObject::connect(&cl,SIGNAL(classback()),&dm, SLOT(get_grade()));		//学生课程返回跳转
